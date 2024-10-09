8f753f2293bc819038952fa7d886ff1f5f43e8c5
Ibe Ifeanyi <ifeanyi-ibe@Ifeanyis-MacBook-Pro.local>
2024-10-09 22:45:22 +0100

    Add features

---
+++ src/PhenGlobal.EmployeeService.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
---
+++ src/PhenGlobal.EmployeeService.Application/Features/LeaveTypes/Queries/GetLeaveTypesRequest.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhenGlobal.EmployeeService.Application.Features.LeaveTypes.Queries
+{
+    public class GetLeaveTypesRequest
+    {
+        
+    }
+}
